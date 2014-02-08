6044053e28281a8e36ea7c06918120ae7579cb15
jongmini <jongmin.choi@gmail.com>
2014-02-08 12:00:07 -0800

    final change

--- bike/app/controllers/sessions_controller.rb
+++ bike/app/controllers/sessions_controller.rb
@@ -6,13 +6,12 @@ class SessionsController < ApplicationController
   def create
     user=User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      redirect_to '/'
     else
       # Create an error message and re-render the signin form.
       flash.now[:error]='Invalid email/password combination'
-      redirect_to '/'
+      render :new
     end
   end
 
